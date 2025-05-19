e59d48b1d1a3a98ea56b98c7d16508041e3e6a4a
Jbaruz <jbaruz@gmail.com>
2025-05-19 18:56:13 -0400

    Add Juan Arevalo instead of Home

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Juan Arevalo' });
 });
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
